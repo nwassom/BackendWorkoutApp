75fdcea092bbd086ef14ccfbe227c58378595939
nwassom <newassom@gmail.com>
2024-04-19 03:37:07 -0400

    push

--- server.ts
+++ server.ts
@@ -1,14 +1,10 @@
 import * as express from 'express';
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
 
 import { connectToDatabase } from './database';
 import AuthRoutes from './routes/AuthRoutes';
-
-dotenv.config();
 
 const app = express();
 connectToDatabase();
@@ -16,7 +12,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(bodyParser.json());
 app.use(express.json())
 app.use(AuthRoutes);
 
