8c09ae510ac03431a2b3e10cea93fb50c5330b35
nwassom <newassom@gmail.com>
2024-04-19 03:25:42 -0400

    push

--- server.ts
+++ server.ts
@@ -14,11 +14,16 @@ const app = express();
 connectToDatabase();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'https://localhost:19002' }));
+app.use(cors());
 
 app.use(bodyParser.json());
-
+app.use(express.json())
 app.use(AuthRoutes);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.listen(port, () => {
 	console.log(`Backend Server listening on port ${port}`);
