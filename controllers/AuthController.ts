c97d4d91ca551aa82cffe145935e75aa2f2f0c6a
nwassom <newassom@gmail.com>
2024-04-19 22:05:07 -0400

    Signup/Login work

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -4,18 +4,18 @@ import jwt from 'jsonwebtoken';
 
 import User, {IUser} from '../models/User';
 
-export const signup = async (req: Request, res: Response): Promise<void> => {
+export const signup = async (req: Request, res: Response): Promise<Response> => {
 
 	console.log(req.body);
 	const { username, email, password, birthday, heightInches, weight, gender, goal } = req.body;
-
 	try 
 	{
 		const hashedPassword = await bcrypt.hash(password, 10);
+		const lowerUser = username.toLowerCase();
 
 		const newUser = new User({
 			email,
-			username,
+			username: lowerUser,
 			password: hashedPassword,
 			birthday,
 			heightInches,
@@ -23,13 +23,51 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
 			gender,
 			goal,
 		});
+
 		await newUser.save();
 
-		res.status(201).json({message: 'User created successfully'});
+		return res.status(201).json({success: true, message: 'User created successfully'});
 	}
 	catch (error)
 	{
 		console.error(error);
-		res.status(500).json({message: 'Server error'});
+		return res.status(500).json({success: false, message: 'Server error'});
 	}
-}
+};
+
+export const login = async (req: Request, res: Response): Promise<Response> => {
+	console.log(req.body);
+
+	const { usernameOrEmail, password } = req.body;
+
+	const lowerCaseUsernameEmail = usernameOrEmail.toLowerCase();
+
+	const userByEmail: IUser | null = await User.findOne({ email: lowerCaseUsernameEmail });
+
+	if (userByEmail)
+	{
+		const isValidPassword = await bcrypt.compare(password, userByEmail.password);
+		if (isValidPassword)
+		{
+			return res.status(200).json({success: true, message: 'Login successful', userByEmail});
+		}
+	}
+
+	const user: IUser | null = await User.findOne({
+		$or: [{ username: lowerCaseUsernameEmail }, { email: lowerCaseUsernameEmail }],
+	});
+
+	console.log(user);
+	if (!user)
+	{
+		return res.status(400).json({success: false, message: 'Invalid Username'});
+	}
+
+	const isValidPassword = await bcrypt.compare(password, user.password);
+	if (!isValidPassword)
+	{
+		return res.status(400).json({success: false, message: 'Invalid Password'});
+	}
+
+	return res.status(200).json({success: true, message: 'Login successful', user});
+};
--- routes/AuthRoutes.ts
+++ routes/AuthRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { signup } from '../controllers/AuthController';
+import { signup, login } from '../controllers/AuthController';
 //import { authenticateUser } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
